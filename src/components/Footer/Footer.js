38e8411a8b28bc04deb2c8759b4afbfa7b9515e1
stas tarasenko <stas23061992@gmail.com>
2020-12-09 10:19:21 +0200

    footer size -

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,51 +1,52 @@
-import React from 'react';
-import logo from '../../resources/images/logo.png';
-import './Footer.scss';
+import React from "react";
+import logo from "../../resources/images/logo.png";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <footer className='footer'>
-      <div className='container'>
-        <div className='row justify-content-center'>
-          <div className='col-lg-3 col-md-4 mb-4 mb-lg-0'>
-            <div className='widget hours'>
-              <h4 className='mb-3'>שעות פתיחה</h4>
+    <footer className="footer">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-3 col-md-4 mb-4 mb-lg-0">
+            <div className="widget hours">
+              <h5 className="mb-3">שעות פתיחה</h5>
 
-              <div className='info'>
-                <p className='mb-0 days'>א,ב,ד,ה:</p>
-                <h5>16:00-19:00 , 08:00-13:00 </h5>
+              <div className="info">
+                <p className="mb-0 days">א,ב,ד,ה:</p>
+                <h6>16:00-19:00 , 08:00-13:00 </h6>
               </div>
-              <div className='info'>
-                <p className='mb-0 days'>ג</p>
-                <h5>08:00-13:00</h5>
+              <div className="info">
+                <p className="mb-0 days">ג</p>
+                <h6>08:00-13:00</h6>
               </div>
-              <div className='info'>
-                <p className='mb-0 days'>ו</p>
-                <h5>07:00-14:00</h5>
+              <div className="info">
+                <p className="mb-0 days">ו</p>
+                <h6>07:00-14:00</h6>
               </div>
             </div>
           </div>
 
-          <div className='col-lg-4 ml-auto col-md-5 mb-5 mb-lg-0'>
-            <div className='widget contacts'>
-              <h4 className='mb-3'>צור קשר</h4>
+          <div className="col-lg-4 ml-auto col-md-5 mb-5 mb-lg-0">
+            <div className="widget contacts">
+              <h5 className="mb-3">צור קשר</h5>
 
-              <ul className='list-unstyled mb-3 footer-contact'>
+              <ul className="list-unstyled mb-3 footer-contact">
                 <li>
-                  089472567 <i className='fas fa-phone-square-alt'></i>
+                  089472567 <i className="fas fa-phone-square-alt"></i>
                 </li>
                 <li>
-                  stas23061992@gmail.com <i className='fas fa-envelope-square'></i>
+                  stas23061992@gmail.com{" "}
+                  <i className="fas fa-envelope-square"></i>
                 </li>
                 <li>
-                  בית הפועלים 5,רחובות <i className='fas fa-home'></i>
+                  בית הפועלים 5,רחובות <i className="fas fa-home"></i>
                 </li>
               </ul>
             </div>
           </div>
 
-          <div className='footer-logo'>
-            <img src={logo} alt='footer-logo' className='img-fluid logo' />
+          <div className="footer-logo">
+            <img src={logo} alt="footer-logo" className="img-fluid logo" />
           </div>
         </div>
       </div>
