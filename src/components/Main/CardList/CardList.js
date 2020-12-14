975910baee0e321ab962527f8436ed02aebc0eb8
stas tarasenko <stas23061992@gmail.com>
2020-12-14 18:16:43 +0200

    main cards

---
+++ src/components/Main/CardList/CardList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import MainCard from "./Card/MainCard.js";
+import image1 from "../../../resources/images/logo.png";
+import "./CardList.scss";
+
+const CardList = () => {
+  return (
+    <div className="cards-container">
+      <MainCard title="title1" text="absafasf" image={image1}></MainCard>
+      <MainCard title="title2" text="aasfa" image={image1}></MainCard>
+      <MainCard
+        title="title3"
+        text="aasfaasfasfaf asfasf saf"
+        image={image1}
+      ></MainCard>
+    </div>
+  );
+};
+
+export default CardList;
